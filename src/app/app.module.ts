56de3c6d87421a94975322574422bf00a442cab7
rishiyanth <apprishiategood002@gmail.com>
2023-01-15 12:55:38 +0530

    Added loader component and service

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,17 +5,24 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { LoginComponent } from './login/login.component';
+import { NewuserComponent } from './newuser/newuser.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import {MatTabsModule} from '@angular/material/tabs';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { NewuserComponent } from './newuser/newuser.component';
+import {MatProgressSpinnerModule} from '@angular/material/progress-spinner';
+import { LoaderComponent } from './loader/loader.component';
+import { HTTP_INTERCEPTORS,HttpClientModule } from '@angular/common/http';
+import { LoaderInterceptor } from '../interceptors/loader.interceptor';
+
+
 
 @NgModule({
   declarations: [
     AppComponent,
     LoginComponent,
-    NewuserComponent
+    NewuserComponent,
+    LoaderComponent
   ],
   imports: [
     BrowserModule,
@@ -24,9 +31,16 @@ import { NewuserComponent } from './newuser/newuser.component';
     BrowserAnimationsModule,
     MatTabsModule,
     FormsModule,
-    ReactiveFormsModule
+    ReactiveFormsModule,
+    MatProgressSpinnerModule
   ],
-  providers: [],
+  providers: [
+    {
+      provide: HTTP_INTERCEPTORS,
+      useClass: LoaderInterceptor,
+      multi: true,
+   },
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
