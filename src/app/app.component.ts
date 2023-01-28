5aa130fba82edafcf167a243009749883facf28f
rishiyanth <apprishiategood002@gmail.com>
2023-01-28 11:24:52 +0530

    Made sidenav as global component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,15 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent{
   title = 'schedio';
+
+  constructor(public router: Router){
+  }
+
 }
