86fabc9820dc1975fd62e15c40e6f581df623073
jagajeet-puttaa <jagajeetputtaa.2002@gmail.com>
2023-03-14 23:49:31 +0530

    Notifications page implementation

--- src/app/notifications/notifications.component.ts
+++ src/app/notifications/notifications.component.ts
@@ -12,4 +12,33 @@ export class NotificationsComponent implements OnInit {
   ngOnInit(): void {
   }
 
+  notificationsData = [
+    {
+      id: 1,
+      time: "5 minutes ago",
+      message: "You have a new friend request from Jane Smith.",
+    },
+    {
+      id: 2,
+      time: "2 hours ago",
+      message: "Your post 'My trip to Hawaii' was liked by Sarah Johnson and 12 others.",
+    },
+    {
+      id: 3,
+      time: "3 days ago",
+      message: "You were mentioned by Lisa Rodriguez in her post 'The best sushi in town'.",
+    },
+    {
+      id: 4,
+      time: "1 week ago",
+      message: "It's Jane Smith's birthday today. Send her a birthday message!",
+    },
+    {
+      id: 5,
+      time: "2 weeks ago",
+      message: "You have a new message from Tom Williams",
+    },
+  ];
+  
+
 }
